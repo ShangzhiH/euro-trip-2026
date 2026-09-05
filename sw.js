f80be0f10bbf6b1d44db8c4ab7e81b609b989be6
Shangzhi HUANG <shangzhi.huang@gmail.com>
2026-09-06 00:51:54 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='europe-trip-cn-v1';
+const APP=['./','./index.html','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.origin===self.location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./index.html'))));
+  }else{
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  }
+});
